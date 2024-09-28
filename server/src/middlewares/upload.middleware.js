b1ba80425d77a27c2c1a34d5bb41685f03c4cfaf
Limon00001 <limonhossain82@gmail.com>
2024-09-28 21:30:25 +0600

    file filtering

--- server/src/middlewares/upload.middleware.js
+++ server/src/middlewares/upload.middleware.js
@@ -1,12 +1,15 @@
 // External Dependencies
 const multer = require('multer');
+const createError = require('http-errors');
 
 // Internal Dependencies
 const path = require('path');
-const { uploadFolder } = require('../secret');
+const { uploadFolder, maxFileSize, allowedFileTypes } = require('../secret');
 
-// Directory
+// Constants
 const UPLOAD_DIR = uploadFolder;
+const MAX_FILE_SIZE = maxFileSize;
+const ALLOWED_FILE_TYPES = allowedFileTypes;
 
 // Multer Configuration
 const storage = multer.diskStorage({
@@ -19,10 +22,27 @@ const storage = multer.diskStorage({
         const finalFileName = fileName.toLowerCase() + '-' + Date.now() + extname;
         cb(null, finalFileName);
     }
-})
+});
+
+// File Filter
+function fileFilter(req, file, cb) {
+    const extname = path.extname(file.originalname);
+    const isMatch = ALLOWED_FILE_TYPES.includes(extname.substring(1));
+    if (!isMatch) {
+        const error = createError(400, `File must be in 'jpg, 'jpeg' and 'png' format.`);
+        return cb(error);
+    }
+    cb(null, true);
+}
 
 // Multer Instance
-const upload = multer({ storage: storage })
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: Number(MAX_FILE_SIZE)
+    },
+    fileFilter
+})
 
 // Module Export
 module.exports = upload;
--- server/src/secret.js
+++ server/src/secret.js
@@ -15,6 +15,8 @@ const smtpUser = process.env.SMTP_USERNAME || '';
 const smtpPassword = process.env.SMTP_PASSWORD || '';
 const clientSite = process.env.CLIENT_SITE;
 const uploadFolder = process.env.UPLOAD_DIR || 'public/images/users';
+const maxFileSize = process.env.MAX_FILE_SIZE || 10 * 1024 * 1024;  // 10 MB
+const allowedFileTypes = process.env.FILE_TYPES || ['png', 'jpg', 'jpeg'];
 
 // Module Export
-module.exports = { port, dbUrl, salt, defaultImagePath, jwtAccessKey, smtpUser, smtpPassword, clientSite, uploadFolder };
+module.exports = { port, dbUrl, salt, defaultImagePath, jwtAccessKey, smtpUser, smtpPassword, clientSite, uploadFolder, maxFileSize, allowedFileTypes };
